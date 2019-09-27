4873eb817d68df554e55e43810d30d45b2b8eea1
Nick Daly <nick.m.daly@gmail.com>
2019-09-26 21:58:46 -0500

    Works, except for small bug syncing back keys on insert.

--- proj1/src/basic-server.py
+++ proj1/src/basic-server.py
@@ -8,6 +8,7 @@ t: timestamp
 """
 
 
+import string
 from collections import defaultdict
 from datetime import datetime
 from pprint import pprint
@@ -15,59 +16,72 @@ from pprint import pprint
 
 class KvEntry:
 
-    def __init__(self, v, t):
+    def __init__(self, v = None, t = None):
         self.v = v
-        self.t = t
+        self.t = datetime.min
+
+    def __repr__(self):
+        return "{} @ {}.{}".format(self.v, self.t.second, self.t.microsecond)
 
 
 class KvServer:
 
+    servers = list()
+
     def __init__(self, me, allServers, initInst):
         self.me = str(me)
         self.kvs = defaultdict(KvEntry) # kvs[key] = [(v: value, t: timestamp), (v:v, t:t), ...]
-        self.servers = defaultdict(int) # s[srv] = [(srv, timestamp), (s, t), ...]
+        self.serverUpdateTimes = defaultdict(int) # s[srv] = [(srv, timestamp), (s, t), ...]
         self.live = 1
 
         for serverPort in allServers:
-            self.servers[str(serverPort)] = initInst
+            self.serverUpdateTimes[str(serverPort)] = initInst
 
     def __str__(self):
-        return "port {} knows {}".format(self.me, self.servers.keys())
+        return "port {} knows {}".format(self.me, self.serverUpdateTimes.keys())
 
     def __repr__(self):
-        return str(self.me) + ":\n" + "\n".join(["    {}: {}:{}.{}".format(
-            k, self.servers[k].minute, self.servers[k].second, self.servers[k].microsecond)
-            for k in self.servers])
+        return ("{}: {} {}\n    {}".format(
+            self.me, "1" if self.live else "0",
+            ["{}: {}".format(k, self.kvs[k]) for k in self.kvs],
+            "\n    ".join([
+                "{}: {}.{}".format(k,
+                                   self.serverUpdateTimes[k].second,
+                                   self.serverUpdateTimes[k].microsecond)
+             for k in self.serverUpdateTimes])))
 
     def insert(self, key, value, time = None):
         if not time:
             time = datetime.now()
         self.kvs[key] = KvEntry(value, time)
-        self.servers[self.me] = time
+        self.serverUpdateTimes[self.me] = time
         self.syncBidi(key, value, time)
 
     def syncBidi(self, k, v, t):
-        for server in servers:
-            if self.servers[sever].live:
+        for server in KvServer.servers:
+            if server.live:
                 self.update(server, server.receive(self.me, k, v, t))
 
-    def receive(sender, k, v, t):
+    def receive(self, sender, k, v, t):
         """A remote server sent us an update.
 
         Find the oldest key we think they have and send them keys that are newer."""
 
+        # update our key if necessary
         if (self.kvs[k].t < t):
-            self.kvs[t].v = v
-            self.kvs[t].t = t
+            self.kvs[k].v = v
+            self.kvs[k].t = t
 
         # we need to play the updates forward from the oldest remote server time.
-        playback = t if t > self.servers[sender] else self.servers[sender]
+        playback = t if t < self.serverUpdateTimes[sender] else self.serverUpdateTimes[sender]
 
+        #import pdb; pdb.set_trace()
         # these are the keys newer than the remote server
-        replay = [k for k in self.kvs if k.t > t]
+        replay = [self.kvs[k] for k in self.kvs if self.kvs[k].t > t]
 
-        # the remote server is now as updated as the most recent update we sent it, or it sent us
-        self.servers[sender] = max([t for replay.t in replay] + KvEntry(k, v, t))
+        # the remote server is now as updated as the most recent update we sent
+        # it, or it sent us, whichever is newer
+        self.serverUpdateTimes[sender] = max([t for replay.t in replay] + [t])
 
         return replay
 
@@ -82,17 +96,49 @@ class KvServer:
             if self.kvs[k].t <= update.t:
                 self.kvs[k] = update
 
-        self.servers[server] = max([t for replay.t in replay]) or self.servers[server]
+        try:
+            self.serverUpdateTimes[server] = max([t for replay.t in replay])
+        except ValueError:
+            # nothing to replay, so the server's update time is already caught up.
+            pass
 
         # oh shoot, the remote sent us back updates that were older than we are right now
         # that means we were out of date, so roll back our clock.
         # TODO: verify servers *can* advance in time and be considered caught up.
-        self.servers[me] = min([t for replay.t in replay] + self.servers[self.me])
+        self.serverUpdateTimes[self.me] = (min([t for replay.t in replay] +
+                                          [self.serverUpdateTimes[self.me]]))
 
 initInst = datetime.now()
-server_ports = list(range(7390, 7400))
-servers = [KvServer(port, server_ports, initInst) for port in server_ports]
-pprint(servers)
+server_ports = list(range(7390, 7393))
+KvServer.servers = [KvServer(port, server_ports, initInst) for port in server_ports]
 
+
+
+alice = KvServer.servers[0]
+bob = KvServer.servers[1]
+calvin = KvServer.servers[2]
+
+alice.insert("a", 1)
+
+print("\n----")
+pprint(KvServer.servers)
+
+bob.live = 0
+
+calvin.insert("a", 2)
+calvin.insert("b", 3)
+
+print("----")
+pprint(KvServer.servers)
+
+bob.live = 1
+bob.insert("c", 10)
+
+print("----")
+pprint(KvServer.servers)
+
+alice.insert("d", 5)
+print("----")
+pprint(KvServer.servers)
 
 # TODO heartbeat: every second, fire a sync from one server to all the others
