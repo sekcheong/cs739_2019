a9ab9da074f99d9d58d31c2ac05b5e301a185928
Nick Daly <nick.m.daly@gmail.com>
2019-10-03 10:19:31 -0500

    Improve performance testing script to output summary.

--- proj1/src/test1.py
+++ proj1/src/test1.py
@@ -11,29 +11,39 @@ old_val = 0
 def main(servers):
     kvlib.init(servers)
 
-    print("command: value (status)")
-
-    status, val = kvlib.put("a", 3)
-    print("put a: {} ({})".format(val, status))
-
-    status, val = kvlib.get("a")
-    print("get a: {} ({})".format(val, status))
+    print("command: (status) value")
+    print("get a: ({}) {}".format(*kvlib.get("a")))
+    print("put a: ({}) {}".format(*kvlib.put("a", 3)))
+    print("get a: ({}) {}".format(*kvlib.get("a")))
 
     # ~666/s on my little laptop.
     # puts will be slower: ~2/s, with autocommit enabled
     # puts: ~50/s without autocommit.
     # this system is optimized for gets, 1200:1
     # performance would benefit greatly by switching to udp
-    print("10 puts, 200 gets per timestamp.")
-    for i in range(1, 10):
+    epochTime = lambda: float(datetime.strftime(datetime.now(), "%s.%f"))
 
-        for j in range(1, 200):
+    gets = 40000
+    puts = 2000
+
+    seconds = 60
+    minutes = 2 * seconds
+    gets = 650 * minutes
+    puts = 45 * minutes
+
+    print(gets, "gets btw timestamps.")
+    start = epochTime()
+    for i in range(0, gets):
             status, val = kvlib.get("a")
+    end = epochTime()
+    print(gets, "gets took", end - start, "seconds. gets per second", gets / (end - start))
 
-        for j in range(1, 10):
-            status, val = kvlib.put("a", 3)
-
-        print(datetime.now())
+    print(puts, "puts btw timestamps.")
+    start = epochTime()
+    for i in range(0, puts):
+        status, val = kvlib.put("a", 3)
+    end = epochTime()
+    print(puts, "gets took", end - start, "seconds. gets per second", puts / (end - start))
 
 if __name__ == "__main__":
     try:
